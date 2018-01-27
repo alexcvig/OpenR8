8f6e3e170547b98b35a1a811b986039b02ec77ca
Mingta <mingta@leaderg.com>
2018-01-27 10:11:19 +0800

    Add OpenR8 Community Edition License.

--- include/R7.hpp
+++ include/R7.hpp
@@ -18,7 +18,7 @@ R7 dll library uses UTF-8 encoding inside.
 //#include "wx/wxprec.h"
 
 
-#define R7_VERSION "18.2.0"
+#define R7_VERSION "18.3.0"
 
 #define R7_STRING_SIZE 4096
 #define R7_TYPE_NAME_SIZE 4096
@@ -122,9 +122,6 @@ extern "C"
 	R7_API int R7_QueueWxEvent(R7CallbackHandler r7CallbackHandler, void *data);
 	R7_API int R7_ProcessWxPendingEvents();
 	R7_API void *R7_GetWxApp();
-	
-	R7_API int R7_Login(char *buffer, int bufferLength, char *userName, char *password);
-	R7_API int R7_CheckLicense(char *licenseKey, int *year, int *month, int *day);
 
 #ifdef __cplusplus
 }
